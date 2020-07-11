8fd51e1055bd1e68c2f0c4fc2f834a4b95aa06b1
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-11 00:48:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 def assign_rooms(speakers)
    assign = []
   speakers.each with index {|speaker,index|}
-  assign << "Hello, #{speaker}! You'll be assigned to room #{index+1}!"
+  assign << "Hello, #{speaker}! You'll be assigned to room #{index}!"
     end
   assign
 end
