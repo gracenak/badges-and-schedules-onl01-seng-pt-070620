831b5b8ba1b3dc3e819a9cff07adac2a6ce1964a
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-11 00:30:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  list = []
+  attendees.each do |attendee|
+    list << "Hello, my name is #{attendee}."
+  end
+   list
+end
+
+def assign_rooms(attendees)
+  array = []
+  attendees.each do |attendee|
+  array << "Hello, #{attendee}. You'll be assigned to room #{attendee.count}"
+  end
+  array
+end
+
+
+    
+    
+    
+ 
